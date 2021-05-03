7eab4012906cedb91a54b42b9701dd2e48179bfa
Florian Clanet <florianclanet@gmail.com>
2021-05-03 23:46:12 +0200

    feat|add phototimer config file

---
+++ config/config.py
@@ -0,0 +1,12 @@
+config = {}
+config["am"] = 800
+config["pm"] = 2000
+
+config["flip_horizontal"] = False
+config["flip_vertical"] = False
+config["metering_mode"] = "matrix"
+
+config["base_path"] = "/home/pi/image"
+config["height"] = 1536
+config["width"] = 2048
+config["quality"] = 35
